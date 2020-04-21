1fc92247571df7509fcec7d6c4ec45b8abacb374
SyedaKissa <60823225+SyedaKissa@users.noreply.github.com>
2020-04-21 16:01:18 +0500

    Update q.cpp

--- q.cpp
+++ q.cpp
@@ -7,29 +7,28 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
  
  char filename1[13]="./myfile.txt";
  int fd1=open(filename1, O_RDWR);
  
  if (fd1==-1)
  {
-   cout<<"File could not open!"<<endl;
+   cout<<"File1 could not open!"<<endl;
    return 1;
  }
  
- cout << "File opened suc\n";
+ cout << "File1 opened successfully\n";
 
  char *map1= (char*) mmap(NULL, 100, PROT_READ, MAP_SHARED, fd1, 0);
  close(fd1); //updated
  
  if (map1==MAP_FAILED)
  {
-   cout<<"File could not be memory mapped!"<<endl;
+   cout<<"File1 could not be memory mapped!"<<endl;
    return 1;
  }
-
+ cout << "File1 mapped successfully\n";
 
 
  //Write in another file
@@ -39,35 +38,38 @@ int main()
  
  if (fd2==-1)
  {
-   cout<<"File could not open!"<<endl;
+   cout<<"File2 could not open!"<<endl;
    return 1;
  }
+ cout << "File2 opened successfully\n";
  
  char *map2= (char*) mmap(NULL, 100, PROT_WRITE, MAP_SHARED, fd2, 0);
  close(fd2); //updated
  
  if (map2==MAP_FAILED)
  {
-   cout<<"File could not be memory mapped!"<<endl;
+   cout<<"File2 could not be memory mapped!"<<endl;
    return 1;
  }
-
+ cout << "File2 opened successfully\n";
 
 //Screen + another file
 
 //ASCI 0=48,  9=57
+ char c;
  int j = 0;
- for (int i=0; i<100; i++)
- {
- 	if(map1[i] >= 48 && map1[i] ){
+ int i;
+ for (i=0; i<100; i++){
+ 	c = map1[i];
+ 	if(c >= 48 && c <= 57 ){
  		cout<<map1[i];
- 		map2[j] = map1[i];
- 	}  
- 	j++; 
+ 		map2[j] = c;
+ 		j++;
+ 	} 	 
  }
  cout<<"Now map2" <<endl;
 
- for(int i = 0; i < 100; i++){
+ for(i = 0; i < 100; i++){
  	cout<<map2[i];
  }
  cout<<endl;
